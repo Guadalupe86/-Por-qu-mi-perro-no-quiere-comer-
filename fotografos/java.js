ffcce5ee9d30a99ae477884466ee7d75d4cca7ae
Guadalupe86 <gantunez895@gmail.com>
2024-11-18 18:49:42 -0600

    Add files via upload

---
+++ fotografos/java.js
@@ -0,0 +1,5 @@
+function navigate(index) {
+    console.log("Navigating to section:", index);
+    // Aquí puedes agregar lógica para cambiar de sección, por ejemplo, mostrar/ocultar contenido según el índice
+    // o utilizar un sistema de enrutamiento si el sitio tiene varias páginas.
+}
